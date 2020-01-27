48848bff9fdaaaa6174c501843afd21687999567
Gabriel Landes <gabe.landes@digitaldefense.com>
2020-01-27 12:02:43 -0600

    add snapshot-ing

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-## EC2 List
-`pipenv run python ec2util/ec2util.py`
+# EC2 Instances Controls
+## List
+`pipenv run python ec2util/ec2util.py instances list`
+
+## Start
+`pipenv run python ec2util/ec2util.py instances start`
+
+## Stop
+`pipenv run python ec2util/ec2util.py instances stop`
+
+## Snapshot
+`pipenv run python ec2util/ec2util.py instances snapshot`
+
+# Volume Controls
+## List
+`pipenv run python ec2util/ec2util.py volumes list`
+
+# Snapshot Controls
+## List
+`pipenv run python ec2util/ec2util.py snapshots list`
+
+## Limit to Project
+Use `--project=NAME` where NAME is the value of the "billingProject" tag to use
+EX: `pipenv run python stop ec2util/ec2util.py instances list --project=some_tag`
+Default value should be set in the environment
--- ec2util/ec2util.py
+++ ec2util/ec2util.py
@@ -4,16 +4,125 @@ import click
 session = boto3.Session(profile_name='default')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+def filter_instances(project):
+    if not project: project = 'gabe-test'
+    filters = [
+        {"Name":"tag:billingProject","Values":[project]}
+    ]
+    inst = ec2.instances.filter(Filters=filters)
+
+    return inst
+
+@click.group()
+def cli():
+    '''CLI commands for EC2 instances'''
+
+@cli.group('instances')
+def instances():
+    '''Commands for instance'''
+
+@cli.group('volumes')
+def volumes():
+    '''Command for volumes'''
+
+@cli.group('snapshots')
+def snapshots():
+    '''Command for snapshots'''
+
+@instances.command('list')
+@click.option('--project', default=None,
+    help="requires a 'billingProject' tag on the instance")
+def list_instances(project):
     '''List ec2 instances'''
-    for i in ec2.instances.filter(Filters=[{"Name":"tag:billingProject","Values":["gabe-test"]}]):
+    inst = filter_instances(project)
+    for i in inst:
+        tags = { t["Key"]: t["Value"] for t in i.tags or []}
         print(','.join((
             i.id,
-            i.instance_type,
-            i.placement['AvailabilityZone'],
+            # i.instance_type,
+            # i.placement['AvailabilityZone'],
             i.state["Name"],
-            i.public_dns_name)))
+            # i.public_dns_name,
+            tags.get('billingProject', '<no project>'))))
+
+@instances.command('stop')
+@click.option('--project', default=None,
+    help="requires a 'billingProject' tag on the instance")
+def stop_instances(project):
+    '''stop ec2 instances'''
+    inst = filter_instances(project)
+    for i in inst:
+        print("Stopping {}...".format(i.id))
+        i.stop()
+
+@instances.command('start')
+@click.option('--project', default=None,
+    help="requires a 'billingProject' tag on the instance")
+def start_instances(project):
+    '''start ec2 instances'''
+    inst = filter_instances(project)
+    for i in inst:
+        print("Starting {}...".format(i.id))
+        i.start()
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="requires a 'billingProject' tag on the attached instance")
+def list_volumes(project):
+    '''List ec2 volumes'''
+    inst = filter_instances(project)
+    for i in inst:
+        for v in i.volumes.all():
+            print(','.join((
+                v.id,
+                i.id,
+                (str(v.size) + 'GB'),
+                # i.instance_type,
+                # i.placement['AvailabilityZone'],
+                v.state,
+                # i.public_dns_name,
+                # tags.get('billingProject', '<no project>')
+            )))
+
+    return
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="requires a 'billingProject' tag on the attached instance")
+def list_snapshots(project):
+    '''List ec2 snapshots'''
+    inst = filter_instances(project)
+    for i in inst:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(','.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.progress,
+                    s.state,
+                    s.start_time.strftime("%c")
+                )))
+
+    return
+
+@instances.command('snapshot')
+@click.option('--project', default=None,
+    help="requires a 'billingProject' tag on the attached instance")
+def snapshot_instances(project):
+    '''snapshot ec2 instances'''
+    inst = filter_instances(project)
+    for i in inst:
+        print("Stopping {}".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+        for v in i.volumes.all():
+            print("Creating snapshot of {}".format(v.id))
+            v.create_snapshot(Description="Created by ec2util (GABE TEST)")
+        print("Starting {}".format(i.id))
+        i.start()
+
+    return
 
 if __name__ == '__main__':
-    list_instances()
+    cli()
